fcf0eb7b03c1775fd914e040875f5652b980c6ed
MarieSerroyen <marie.serroyen@gmail.com>
2023-04-30 14:44:51 +0200

    fix: errors upload image

--- src/components/UploadClothing.vue
+++ src/components/UploadClothing.vue
@@ -1,42 +1,11 @@
 <script lang="ts" setup>
     import { ref} from 'vue'
-    //import { Blob } from 'buffer'
 
-    /*let imageUrl = ref('');
-
-    function setImage(url) {
-        imageUrl.value = url;
-    }*/
-
-    /*const uploadImage = () => {
-        const file: HTMLInputElement = document.getElementById("fileUpload") as HTMLInputElement;
-        //const file = imagesUpload?.current?.files?.[0];
-        const blob = new Blob(file, { type: "image/png" });
-        //const [imageUrl, setImage] = useState<string | ArrayBuffer | null>(null);
-        let formData = new FormData();
-
-        formData.append("file", blob);
-        formData.append("upload_preset", import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
-
-        fetch(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_USER_NAME}/image/upload`, {
-            method: "POST",
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            //setImage(data.secure_url);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }*/
-
-    //const imagesUpload = ref(null);
     let imageUrl = ref('');
 
-    const uploadImage = (e: { target: { files: any[]; }; }) => {
-        const file = e.target.files[0];
+    const uploadImage = (e: Event) => {
+        const target = e.target as HTMLInputElement;
+        const file = target.files![0];
         let formData = new FormData();
 
         formData.append("file", file);
