27c6f66c2e5b28d0fd1891bc167e3d49a92d0595
MarieSerroyen <marie.serroyen@gmail.com>
2023-06-05 10:20:03 +0200

    style: product list

---
+++ src/components/ProductListItems.vue
@@ -0,0 +1,67 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="section">
+        <div class="titles">
+            <h3 class="title">Product id</h3>
+            <h3 class="title">Added</h3>
+            <h3 class="title">Category</h3>
+            <h3 class="title">Price</h3>
+        </div>
+
+        <div class="items">
+            <p class="item name">Name</p>
+            <p class="item">Date</p>
+            <p class="item">Category</p>
+            <p class="item">Price</p>
+            <img src="../assets/dropdown-arrow.svg" alt="">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .section {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+    }
+
+    .titles {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        border-bottom: solid 1px #CCCCCC;
+        width: 100%;
+        gap: 1rem;
+      }
+
+    .title {
+        font-size: 16px;
+        color: #5C5F66;
+        font-weight: 500;
+        text-transform: uppercase;
+    }
+
+    .name {
+        font-size: 15px;
+        color: #000000;
+        font-weight: 600;
+    }
+
+    .items {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        border-bottom: solid 1px #CCCCCC;
+        width: 100%;
+        align-items: center;
+    }
+
+    .item {
+        font-size: 15px;
+        color: #000000;
+        font-weight: 400;
+    }
+
+</style>
--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
     import Nav from '../components/NavComponent.vue'
+    import ProductList from '../components/ProductListItems.vue'
 
 </script>
 
@@ -9,6 +10,44 @@
     <div class="content">
         <div class="header">
             <h1 class="title">Product list</h1>
+        </div>
+
+        <div class="tabs">
+            <div class="tab-item">
+                <h4 class="tab-title active">All products</h4>
+            </div>
+            <div class="tab-item">
+                <h4 class="tab-title">Tab</h4>
+            </div>
+            <div class="tab-item">
+                <h4 class="tab-title">Tab</h4>
+            </div>
+            <div class="tab-item">
+                <h4 class="tab-title">Tab</h4>
+            </div>
+            <div class="tab-item">
+                <h4 class="tab-title">Tab</h4>
+            </div>
+            <div class="tab-item">
+                <h4 class="tab-title">Tab</h4>
+            </div>
+            <div class="tab-item">
+                <h4 class="tab-title">Tab</h4>
+            </div>
+            <div class="tab-item">
+                <h4 class="tab-title">Tab</h4>
+            </div>
+
+        </div>
+
+        <hr class="line">
+
+        <div class="search">
+            <input class="search-input" type="text" placeholder="Search by order id">
+        </div>
+
+        <div class="list">
+            <ProductList />
         </div>
     </div>
 
@@ -35,6 +74,47 @@
         font-size: 24px;
         color: black;
         text-transform: capitalize;
+    }
+
+    .tabs {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        width: 100%;
+        gap: 2rem;
+    }
+
+    .tab-title{
+        font-size: 15px;
+        color: #5C5F66;
+        text-transform: capitalize;
+        font-weight: 400;
+    }
+
+    .active {
+        color: #0F60FF;
+        border-bottom: solid 2px #0F60FF;
+    }
+
+    .line {
+        width: 100%;
+        border: solid 1px #CCCCCC;
+    }
+
+    .search-input {
+        background-image: url('../assets/search.svg');
+        background-repeat: no-repeat;
+        background-position: 10px center;
+        background-size: 15px;
+        padding-left: 40px;
+        border: solid 1px #CCCCCC;
+        border-radius: 5px;
+        width: 100%;
+        height: 40px;
+        font-size: 15px;
+        color: #5C5F66;
+        font-weight: 400;
     }
 
 </style>
