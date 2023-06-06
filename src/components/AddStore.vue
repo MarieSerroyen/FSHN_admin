5a943dede307ef7e24f5f6dc1fbbfa117648ed1e
MarieSerroyen <marie.serroyen@gmail.com>
2023-06-06 10:21:44 +0200

    style: store input form

---
+++ src/components/AddStore.vue
@@ -0,0 +1,97 @@
+<script lang="ts" setup>
+    import { ref } from 'vue'
+
+    const name = ref('');
+    const email = ref('');
+    const phone = ref('');
+
+</script>
+
+<template>
+    <div class="upload">
+        <h2 class="subtitle">Store information</h2>
+        <p class="section_description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore.
+        </p>
+
+        <div class="input_grid">
+            <div class="text_input">
+                <label for="name">Store name</label>
+                <input class="inputfield" type="text" id="name" name="name" v-model="name">
+            </div>
+
+            <div class="text_input">
+                <label for="email">Mail</label>
+                <input class="inputfield" type="text" id="email" name="email" v-model="email">
+            </div>
+
+            <div class="text_input">
+                <label for="phone">Phone</label>
+                <input class="inputfield" type="text" id="phone" name="phone" v-model="phone">
+            </div>
+        </div>
+
+        <div class="button-section">
+            <a class="button">Submit</a>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+    label {
+        font-size: 16px;
+        color: black;
+        font-weight: 700;
+    }
+    .subtitle {
+        font-size: 18px;
+        color: black;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
+        text-transform: capitalize;
+    }
+
+    .input_grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 4rem;
+    }
+
+    .text_input {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
+        width: 100%;
+        gap: 1rem;
+        margin-bottom: 1rem;
+        margin-top: 1rem;
+    }
+
+    .inputfield {
+        height: 30px;
+        width: 100%;
+    }
+
+    .button-section {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    .button {
+        background-color: #000000;
+        color: #ffffff;
+        padding: 0.5rem 1rem;
+        font-size: 16px;
+        font-weight: 700;
+        text-transform: uppercase;
+        margin-top: 1rem;
+    }
+
+</style>
--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,11 +1,44 @@
 <script lang="ts" setup>
+    import Nav from '../components/NavComponent.vue'
+    import AddStore from '../components/AddStore.vue'
 
 </script>
 
 <template>
-    lfsmkflmqkfsd
+    <Nav/>
+
+    <div class="header">
+        <h1 class="title">Add Stores</h1>
+    </div>
+
+    <div class="uploads">
+        <div>
+            <AddStore/>
+        </div>
+
+    </div>
 </template>
 
 <style scoped>
+    .header {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        gap: 2rem;
+    }
+    .title {
+        font-size: 24px;
+        color: black;
+    }
 
+    .uploads {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
+        width: 100%;
+        gap: 2rem;
+    }
 </style>
