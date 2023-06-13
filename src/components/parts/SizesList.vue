1b95f72b78fa562df04b5cc7d124a2abe4efb412
MarieSerroyen <marie.serroyen@gmail.com>
2023-06-13 13:35:35 +0200

    style: multiselect dropdown sizes

--- src/components/parts/SizesList.vue
+++ src/components/parts/SizesList.vue
@@ -44,15 +44,32 @@
             //console.log (selectedSizes.value);
         }
     }
+
+    const handleClick = (_e: Event) => {        
+        const select = document.querySelector('.options');
+        select?.classList.toggle('hidden');
+    }
 </script>
 
 <template>
     <div class="sizes_section">
         <p>Sizes</p>
-        <div class="sizes" v-for="(size, key) in sizes" :key="key" >
+        <div class="multiselect" @click="handleClick">
+            <div class="options hidden">
+                <div class="option" v-for="(size, key) in sizes" :key="key">
+                    <div class="checkbox">
+                        <input type="checkbox" :id="size" :name="size" :value="size" class="size" @change="selectSize">
+                    </div>
+                    <div class="text">
+                        {{ size }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--<div class="sizes" v-for="(size, key) in sizes" :key="key" >
             <input type="checkbox" :id="size" :name="size" :value="size" class="size" @change="selectSize">
             <label :for="size">{{ size }}</label>
-        </div>
+        </div>-->
 
     </div>
 
@@ -72,6 +89,57 @@
         justify-content: flex-start;
         align-items: flex-start;
         width: 100%;    
+    }
+
+    .multiselect{
+        background: #FFFFFF;
+        padding: 6px, 12px;
+        border: 1px solid #E5E7EB;
+        display: inline-block; 
+        border-radius: 7.41186px;
+        box-sizing: border-box;
+        min-height: 33px;
+        min-width: 222px;
+        position: relative;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .multiselect:focus {
+        outline: none;
+    }
+
+    .options {
+        position: absolute;
+        top: 34px;
+        right: 0;
+        left: 0;
+        display: flex;
+        background: white;
+        flex-direction: column;
+        min-height: 55px;
+        max-height: 188px;
+        overflow-y: auto;
+    }
+
+    .option {
+        padding: 6px 11px;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+    }
+
+    .size {
+        width: 15px;
+        height: 15px;
+        border: 1px solid #E5E7EB;
+        margin-right: 7px;
+        position: relative;
+        
+    }
+
+    .hidden {
+        display: none;
     }
     
 
